485f906e1cda98f4c07970961eabf5cc60752378
Jason Jacobs <jayj3.0pro@gmail.com>
2023-07-20 17:48:45 -0600

    RPS game

--- src/app/page/projects/arcade/rockpaperscisors/rockpaperscisors.page.ts
+++ src/app/page/projects/arcade/rockpaperscisors/rockpaperscisors.page.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RockpaperscisorsPage implements OnInit {
 
+  playerChoice: string = '';
+  computerChoice: string = '';
+  result: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  selectOption(choice: string): void {
+    this.playerChoice = choice;
+    this.computerChoice = this.generateComputerChoice();
+    this.calculateResult();
+  }
+
+  generateComputerChoice(): string {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  }
+
+  calculateResult(): void {
+    if (this.playerChoice === this.computerChoice) {
+      this.result = "It's a tie!";
+    } else if (
+      (this.playerChoice === 'rock' && this.computerChoice === 'scissors') ||
+      (this.playerChoice === 'paper' && this.computerChoice === 'rock') ||
+      (this.playerChoice === 'scissors' && this.computerChoice === 'paper')
+    ) {
+      this.result = 'You win!';
+    } else {
+      this.result = 'Computer wins!';
+    }
+  }
+
+  resetGame(): void {
+    this.playerChoice = '';
+    this.computerChoice = '';
+    this.result = '';
+  }
 }
