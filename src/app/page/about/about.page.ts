868f3099a21e2ee8b27586b0ed51139ec3a63f74
Jason Jacobs <jayj3.0pro@gmail.com>
2023-08-28 23:30:54 -0600

    about-page-navbar

--- src/app/page/about/about.page.ts
+++ src/app/page/about/about.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavMenuService } from 'src/app/nav-menu.service';
+import { LoginService } from 'src/app/login.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPage implements OnInit {
 
-  constructor() { }
+
+  constructor(
+    private _navMenuService: NavMenuService,
+    private _loginService: LoginService) { }
 
   ngOnInit() {
+  }
+
+  currentUser(){
+    return this._loginService.getUser();
+  }
+  
+  issignedIn() {
+    return this._loginService.isSignedIn();
+  }
+
+  
+  
+
+  home() {
+    this._navMenuService.home();
+  }
+
+  arcade() {
+    this._navMenuService.arcade();
+  }
+
+  office() {
+    this._navMenuService.office();
+  }
+
+  projects() {
+    this._navMenuService.projects();
+  }
+
+  skills() {
+    this._navMenuService.skills()
+  }
+
+  signUp() {
+
+  }
+
+  logout() {
+    this._navMenuService.welcome();
+    this._loginService.logout();
   }
 
 }
